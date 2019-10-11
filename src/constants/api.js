a109a393b430e8403dd8900aeb66f36b6cd96279
Maxime Aubanel <maximeauba@gmail.com>
2019-10-11 13:03:12 +0200

    :construction: Change API url

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,9 +1,9 @@
 export const API_ROOT =
 	process.env.NODE_ENV === "development"
 		? "http://localhost:8080/api"
-		: "https://skibble.herokuapp.com/api";
+		: "https://skibble-backend-prod.herokuapp.com/api";
 
 export const WS_ROOT =
 	process.env.NODE_ENV === "development"
 		? "http://localhost:8080/lobby"
-		: "https://skibble.herokuapp.com/lobby";
+		: "https://https://skibble-backend-prod.herokuapp.com/lobby";
