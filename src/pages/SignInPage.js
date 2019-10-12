12bef46f1bfe2177b4737edee9a316926f157f95
theochampion <contact.theochampion@gmail.com>
2019-10-12 17:44:39 +0200

    :rotating_light: Lint

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,99 +1,99 @@
-import React from "react";
-import styled from "styled-components";
-import Button from "../components/Button";
-import StyledLink from "../components/Link";
-import Spacer from "../components/Spacer";
-import Form from "../components/Form";
-import { SIGNUP_ROUTE } from "../constants/routes";
-import { withRouter } from "react-router-dom";
-import { toast } from "react-toastify";
-import { withUser } from "../providers/UserProvider";
+import React from 'react';
+import styled from 'styled-components';
+import Button from '../components/Button';
+import StyledLink from '../components/Link';
+import Spacer from '../components/Spacer';
+import Form from '../components/Form';
+import { SIGNUP_ROUTE } from '../constants/routes';
+import { withRouter } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { withUser } from '../providers/UserProvider';
 
 const Content = styled.div`
-	width: 25em;
-	display: flex;
-	flex-direction: column;
-	align-items: flex-start;
-	justify-content: center;
+  width: 25em;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
 `;
 
 const Title = styled.h1`
-	color: ${props => props.theme.primary};
-	font-weight: normal;
-	font-style: italic;
-	letter-spacing: 0px;
-	line-height: 1.2em;
-	font-family: "Poppins", sans-serif;
-	font-size: 4em;
-	font-weight: 600;
-	font-style: normal;
-	letter-spacing: -0.01em;
-	line-height: 1.3em;
-	text-transform: none;
-	text-decoration: none;
-	margin: 0;
-	@media (max-width: ${({ theme }) => theme.tablet}) {
-		font-size: 3em;
-	}
+  color: ${props => props.theme.primary};
+  font-weight: normal;
+  font-style: italic;
+  letter-spacing: 0px;
+  line-height: 1.2em;
+  font-family: 'Poppins', sans-serif;
+  font-size: 4em;
+  font-weight: 600;
+  font-style: normal;
+  letter-spacing: -0.01em;
+  line-height: 1.3em;
+  text-transform: none;
+  text-decoration: none;
+  margin: 0;
+  @media (max-width: ${({ theme }) => theme.tablet}) {
+    font-size: 3em;
+  }
 `;
 
 class SignInPage extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = { username: "", password: "" };
-	}
+  constructor(props) {
+    super(props);
+    this.state = { username: '', password: '' };
+  }
 
-	_handleFormChange = event => {
-		this.setState({ [event.target.name]: event.target.value });
-	};
+  _handleFormChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
-	_handleFormSubmit = async event => {
-		event.preventDefault();
-		try {
-			await this.props.login(this.state.username, this.state.password);
-			this.props.history.push("/");
-		} catch (e) {
-			toast.error(e, {
-				position: toast.POSITION.TOP_CENTER
-			});
-		}
-	};
+  _handleFormSubmit = async event => {
+    event.preventDefault();
+    try {
+      await this.props.login(this.state.username, this.state.password);
+      this.props.history.push('/');
+    } catch (e) {
+      toast.error(e, {
+        position: toast.POSITION.TOP_CENTER
+      });
+    }
+  };
 
-	render() {
-		return (
-			<Content>
-				<Title>Sign In</Title>
-				<Spacer />
-				<Form onSubmit={this._handleFormSubmit}>
-					<label htmlFor="username">Username</label>
-					<input
-						name="username"
-						type="text"
-						placeholder="Enter your username"
-						value={this.state.username}
-						onChange={this._handleFormChange}
-					/>
-					<label>Password</label>
-					<input
-						name="password"
-						type="password"
-						placeholder="Enter your password"
-						value={this.state.password}
-						onChange={this._handleFormChange}
-					/>
-					<Spacer />
+  render() {
+    return (
+      <Content>
+        <Title>Sign In</Title>
+        <Spacer />
+        <Form onSubmit={this._handleFormSubmit}>
+          <label htmlFor="username">Username</label>
+          <input
+            name="username"
+            type="text"
+            placeholder="Enter your username"
+            value={this.state.username}
+            onChange={this._handleFormChange}
+          />
+          <label>Password</label>
+          <input
+            name="password"
+            type="password"
+            placeholder="Enter your password"
+            value={this.state.password}
+            onChange={this._handleFormChange}
+          />
+          <Spacer />
 
-					<Button>Sign in</Button>
-				</Form>
-				<Spacer />
+          <Button>Sign in</Button>
+        </Form>
+        <Spacer />
 
-				<Spacer>
-					Don't have an account?&nbsp;
-					<StyledLink to={SIGNUP_ROUTE}>Sign up</StyledLink>
-				</Spacer>
-			</Content>
-		);
-	}
+        <Spacer>
+          Don't have an account?&nbsp;
+          <StyledLink to={SIGNUP_ROUTE}>Sign up</StyledLink>
+        </Spacer>
+      </Content>
+    );
+  }
 }
 
 export default withRouter(withUser(SignInPage));
