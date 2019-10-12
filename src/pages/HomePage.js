d7126709c8538e9001ab99130d7a577a810f9e59
theochampion <contact.theochampion@gmail.com>
2019-10-12 18:38:25 +0200

    :sparkles: Added room selection in lobby

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,77 +1,125 @@
-import React from "react";
-import { withRouter } from "react-router-dom";
-import styled from "styled-components";
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import styled from 'styled-components';
+import io from 'socket.io-client';
+// providers
+import { withUser } from '../providers/UserProvider';
 // components
-import SingleFieldForm from "../components/SingleFieldForm";
-import Button from "../components/Button";
-import Spacer from "../components/Spacer";
-import { SIGNIN_ROUTE } from "../constants/routes";
-import { UserConsumer } from "../providers/UserProvider";
+import SingleFieldForm from '../components/SingleFieldForm';
+import Button from '../components/Button';
+import Spacer from '../components/Spacer';
+import StyledLink from '../components/Link';
+
+// constants
+import { WS_ROOT } from '../constants/api';
+import { SIGNIN_ROUTE } from '../constants/routes';
+import { SERVER_EVENT, CLIENT_EVENT } from '../constants/events';
+import List from '../components/List';
 
 const Content = styled.div`
-	width: 25em;
-	display: flex;
-	flex-direction: column;
-	align-items: flex-start;
-	justify-content: center;
+  width: 25em;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
 `;
 
 const Description = styled.p`
-	font-family: "Poppins", sans-serif;
-	font-size: 1em;
-	color: ${props => props.theme.text};
+  font-family: 'Poppins', sans-serif;
+  font-size: 1em;
+  color: ${props => props.theme.text};
 `;
 
 const Title = styled.h1`
-	color: ${props => props.theme.primary};
-	font-size: 36px;
-	font-weight: normal;
-	font-style: italic;
-	letter-spacing: 0px;
-	line-height: 1.2em;
-	font-family: "Poppins", sans-serif;
-	font-size: 5em;
-	font-weight: 600;
-	font-style: normal;
-	letter-spacing: -0.01em;
-	line-height: 1.3em;
-	text-transform: none;
-	text-decoration: none;
-	margin: 0;
-	@media (max-width: ${({ theme }) => theme.tablet}) {
-		font-size: 3em;
-	}
+  color: ${props => props.theme.primary};
+  font-family: 'Poppins', sans-serif;
+  font-size: 5em;
+  font-weight: 600;
+  font-style: normal;
+  line-height: 1.3em;
+  text-transform: none;
+  text-decoration: none;
+  margin: 0;
+  @media (max-width: ${({ theme }) => theme.tablet}) {
+    font-size: 3em;
+  }
 `;
 
-const HomePage = withRouter(({ history }) => (
-	<UserConsumer>
-		{({ isLogged }) => (
-			<Content>
-				<Title>Skible</Title>
-				<Description>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-					eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-					minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-					aliquip ex ea commodo consequat. Duis aute irure dolor in
-					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-					pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-					culpa qui officia deserunt mollit anim id est laborum.
-				</Description>
-				<Spacer />
+const SubTitle = styled.h2`
+  color: ${props => props.theme.text};
+  font-size: 1.4rem;
+  font-weight: bold;
+  line-height: 1.2em;
+  font-family: 'Poppins', sans-serif;
+`;
 
-				{isLogged ? (
-					<>
-						<SingleFieldForm
-							placeholder="Join or create a room"
-							onSubmit={value => history.push(`/room/${value}`)}
-						/>
-					</>
-				) : (
-					<Button onClick={() => history.push(SIGNIN_ROUTE)}>Sign In</Button>
-				)}
-			</Content>
-		)}
-	</UserConsumer>
-));
+class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
 
-export default HomePage;
+    this.state = {
+      rooms: []
+    };
+  }
+
+  componentDidMount() {
+    this.socket = io(WS_ROOT);
+    this.socket.emit(SERVER_EVENT.LOGIN, {
+      username: this.props.username,
+      password: this.props.password,
+      token: this.props.token
+    });
+
+    this.socket.on(CLIENT_EVENT.UPDATE_ROOMS, data => {
+      this.setState({ rooms: data });
+    });
+  }
+  render() {
+    return (
+      <Content>
+        <Title>Skible</Title>
+        {this.props.isLogged ? (
+          <>
+            <SubTitle>Join room</SubTitle>
+            {this.state.rooms.length > 0 ? (
+              <List>
+                {this.state.rooms.map(room => (
+                  <li key={room.name}>
+                    <span>{`#${room.name} (${room.userNb} / 10)`}</span>
+                    <StyledLink to={`/room/${room.name}`}>Join</StyledLink>
+                  </li>
+                ))}
+              </List>
+            ) : (
+              <Description>No room existing rooms</Description>
+            )}
+            <Spacer>OR</Spacer>
+            <SubTitle>Create room</SubTitle>
+            <SingleFieldForm
+              placeholder="Room name..."
+              onSubmit={value => this.props.history.push(`/room/${value}`)}
+            />
+          </>
+        ) : (
+          <>
+            <Description>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Description>
+            <Button onClick={() => this.props.history.push(SIGNIN_ROUTE)}>
+              Sign In
+            </Button>
+          </>
+        )}
+        <Spacer />
+      </Content>
+    );
+  }
+}
+
+export default withRouter(withUser(HomePage));
