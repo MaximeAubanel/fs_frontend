eb10fbec2eae280bb91f086fc87a968facc95c80
theochampion <contact.theochampion@gmail.com>
2019-10-12 17:41:46 +0200

    :art: Simplify URL definition

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,13 +1,10 @@
-export const API_ROOT =
-	process.env.NODE_ENV === "development"
-		? "http://localhost:8080/api"
-		: process.env.BACK_ENV === "development"
-			? "https://skibble-backend-dev.herokuapp.com/api"
-			: "https://skibble-backend-prod.herokuapp.com/api"
+export const ROOT_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:8080'
+    : process.env.BACK_ENV === 'development'
+    ? 'https://skibble-backend-dev.herokuapp.com'
+    : 'https://skibble-backend-prod.herokuapp.com';
 
-export const WS_ROOT =
-	process.env.NODE_ENV === "development"
-		? "http://localhost:8080/lobby"
-		: process.env.BACK_ENV === "development"
-			? "https://skibble-backend-dev.herokuapp.com/lobby"
-			: "https://skibble-backend-prod.herokuapp.com/lobby"
+export const API_ROOT = `${ROOT_URL}/api`;
+
+export const WS_ROOT = `${ROOT_URL}/lobby`;
